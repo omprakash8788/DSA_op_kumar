7e3c5b483247848e65a3a60d51dfed305a38e97c
omprakash8788 <omprakashkumar552@gmail.com>
2023-06-14 22:07:22 +0530

    add

--- prob66.js
+++ prob66.js
@@ -1,30 +1,17 @@
 function findPattern(str, pat) {
     const n = str.length;
     const m = pat.length;
-  
-    // Loop through the string
     for (let i = 0; i <= n - m; i++) {
       let j;
-  
-      // Check if the current substring matches the pattern
       for (j = 0; j < m; j++) {
         if (str[i + j] !== pat[j]) {
           break;
         }
       }
-  
-      // If the pattern is found, return 'yes'
       if (j === m) {
-        return 'yes';
+        console.log("Yes");
       }
     }
-  
-    // If no matching substring is found, return 'no'
-    return 'no';
+    console.log("No");
   }
-  
-  // Test case
-  const str = 'abcbababaa';
-  const pat = 'cab';
-  console.log(findPattern(str, pat));
   
