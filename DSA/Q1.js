ab6238925bf92dd2df30ceb32b949cb0f2dd4139
omprakash8788 <omprakashkumar552@gmail.com>
2023-08-29 23:16:40 +0530

    add

---
+++ DSA/Q1.js
@@ -0,0 +1,21 @@
+
+//     The Senior Citizen
+//      Description
+//      You are given a number stored in a variable, with the name age
+//      Check whether the age is greater than and equal to 60 or not
+//      If true, Then print "Senior Citizen"
+//      otherwise "Not Senior Citizen"
+//     Output
+//     If the value stored in age , is greater than and equal to 60 Then print Senior Citizen, else print not Senior Citizen
+//     Hint
+//     the sample input, age = 50
+//     Since 50 is less than 60.
+
+let age = 50 ;
+if(age>=60){
+    console.log("Senior Citizen");
+}
+else{
+    console.log("Not Senior Citizen");
+}
+
