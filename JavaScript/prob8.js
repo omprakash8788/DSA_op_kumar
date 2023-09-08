33226af1fe9824a59549b7be50dc8f22e644df51
omprakash8788 <omprakashkumar552@gmail.com>
2023-09-08 22:09:04 +0530

    add

--- JavaScript/prob8.js
+++ JavaScript/prob8.js
@@ -1,23 +1,25 @@
-// Remove duplicates
-// Given an array of n integers, the array is sorted. You have to remove the duplicates, print only unique elements.
-let n=6
-let arr =[1,1,2,2,4,3]
+// Given an array of size N. The task is to find the maximum and the minimum element of the array using the minimum number of comparisons.
+//   Input arr=[3,5,4,1,9]
+// Output: Minimum element is: 1
+//         Maximum element is: 9
+//  Input:  arr=[22, 14, 8, 17, 35, 3]
+// Output:  Minimum element is: 3
+//          Maximum element is: 35
+let arr = [3,5,4,1,9]
+let min=arr[0]
+let max=arr[0]
+for(let i=0; i<arr.length; i++){
+    if(arr[i]<min){
+        min=arr[i]
+    }
+    else if(arr[i]>max){
+        max=arr[i]
+    }
+}
+console.log("Minimum element is:",min);
+console.log("Maximum element is:",max);
 
-let obj={}
-for(let i=0; i<n; i++){
-    if(obj[arr[i]]==undefined){
-        obj[arr[i]]=1;
-    }
-    else{
-         obj[arr[i]]++
-    }
-}
-let count=0;
-let bag="";
-for(let key in obj){
-    if(obj[key]>=1){
-        count++;
-        bag=bag+key+" "
-    }
-}
-console.log(bag)
+
+
+
+
