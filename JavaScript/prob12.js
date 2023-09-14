e5e4a54cf16cdf48d119cc7aef4fc2f31dcc5542
omprakash8788 <omprakashkumar552@gmail.com>
2023-09-14 19:26:27 +0530

    add

--- JavaScript/prob12.js
+++ JavaScript/prob12.js
@@ -1,24 +1,34 @@
 // Count number of common elements between a sorted array and a reverse sorted array
 // Given two arrays consisting of N distinct integers such that the array A[] and B[] are sorted in ascending and descending order respectively, the task is to find the number of values common in both arrays.
 let N=6;
-let arr1=[1,2,2,3,4,5]
-let arr2=[4,4,3,2,1,1]
-
-// Output = 4;
-let l=0;
-let r=N-1;
+let arr1=[1,2,2,3,4,5]             // arr1=[1,2,2,3,4,5]  ,  arr2=[4,4,3,2,1,1] 
+let arr2=[4,4,3,2,1,1] 
+// Output - 4 
 let count=0;
-while(l<N && r>=0){
-    if(arr2[r]==arr1[l]){
-        count++;
-        l++;
-        r--
+let L=0;
+let R=N-1;
+while(L<N && R>=0){
+    if(arr2[R]==arr1[L]){
+        count++
+        L++
+        R--
     }
-    else if(arr1[l]<arr2[r]){
-        l++
+    else if(arr1[L]<arr2[R]){
+        L++
     }
     else{
-        r--
+        R--
     }
 }
 console.log(count);
+
+
+
+
+
+
+
+
+
+
+
