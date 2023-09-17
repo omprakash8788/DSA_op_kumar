b22a47329f772d8a089994552bf93dd4081caa86
omprakash8788 <omprakashkumar552@gmail.com>
2023-09-17 23:28:39 +0530

    add

--- JavaScript/prob18.js
+++ JavaScript/prob18.js
@@ -1,4 +1,4 @@
-//Equal to 52 or Not 
+//Equal to 30 or Not 
 // You are given an array of N integer . You need to print YES if 30 is present in array ,else print NO
 //  Input arr=[4,7,9,8,3]
 //  N=5;
@@ -6,16 +6,18 @@
 // Output NO
 let arr=[4,7,9,8,3]
 let N=5;
+let k=30;
 let count=0;
 for(let i=0; i<N; i++){
-    if(arr[i]===52){
+    if(arr[i]===k){
         count++
     }
 }
 if(count>1){
-    console.log("YES");
+    console.log("Yes");
 }
 else{
-    console.log("NO");
+    console.log("No");
 }
+
 
